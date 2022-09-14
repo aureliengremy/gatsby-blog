3348c366e8cddb3e6fc676f99228d6e1d8d88154
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-09-14 17:09:18 -0400

    create page - blog - and add link from home and blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>
+        <h1>Blog</h1>
+        <Link to="/home">Back to home page</Link>
+    </div>
+  )
+}
+
+export default Blog
